b24f127451d603d893bb1538ca92c0d434e2b7c1
Mark Fischer <fischerm@email.arizona.edu>
2016-02-27 13:04:36 -0700

    Added Homework 4 files

--- examples/js/jstest.js
+++ examples/js/jstest.js
@@ -1,5 +1,23 @@
-﻿var answer = 42;
-function calculateAnswer() {
-  return answer;
+var doubleMe = function(x) {
+  return 2 * x;
 }
-console.log( calculateAnswer() );
+
+var halveMe = function(x) {
+  return x/2;
+}
+
+var myLib = {
+  version: 0.3,
+  name: "My Test Library",
+  double: doubleMe,
+  half: halveMe
+}
+
+console.log( myLib.double(3) );
+console.log( myLib.half(10) );
+
+myLib.half = function(x) {
+    return x/3;
+}
+
+console.log( myLib.half(9) );
